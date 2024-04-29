f8270dfc2a37791ae6ae4d441c4858bde4cedf82
Artur Flis <leeos10100@gmail.com>
2024-04-29 19:31:00 +0200

    Add .env file to .gitignore and load it in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'src/app.dart';
 
 void main() async {
-
+  await dotenv.load(fileName: ".env");
   runApp(MyApp(key: UniqueKey()));
 }
