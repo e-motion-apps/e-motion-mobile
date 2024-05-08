11869779b71ecee2bcea727aa2df3f2b315635dc
Artur Flis <leeos10100@gmail.com>
2024-05-08 18:40:46 +0200

    lint

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load();
   runApp(MyApp(key: UniqueKey()));
 }
