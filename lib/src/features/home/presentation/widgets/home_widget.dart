f0c496b3501efbdc8933effc8edaae1c36c3f52b
Artur Flis <leeos10100@gmail.com>
2024-05-09 18:32:40 +0200

    chore: Add menu button and emotion image to home page AppBar

--- lib/src/features/home/presentation/widgets/home_widget.dart
+++ lib/src/features/home/presentation/widgets/home_widget.dart
@@ -6,6 +6,19 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Escooters Home'),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            // Add your menu button functionality here
+          },
+        ),
+        actions: [
+          Image.asset(
+            'assets/emotion-76.png',
+            width: 40,
+            height: 40,
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Welcome to the Escooters Home Page!'),
